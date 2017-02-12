4141d949e640466cc3cfeafcec8fa22743352587
Your Name <you@example.com>
2017-02-12 09:05:51 +0800

    first

--- js/nowplaying.js
+++ js/nowplaying.js
@@ -1,4 +1,11 @@
 angular.module('movieApp.nowplayingCtrl',[])
-    .controller('nowplayingCtrl',["$scope",function($scope){
-      
+    .controller('nowplayingCtrl',["$scope",'$http',function($scope,$http){
+      //创建电影列表的展示功能
+      $scope.movie = {};
+      //通过ajax请求data.json文件  获取数据赋值给movie属性
+      $http({
+        url:'data.json'
+      }).success(function(data){
+        $scope.movie = data;
+      })
     }])
--- js/route.js
+++ js/route.js
@@ -1,4 +1,16 @@
 angular.module('movieApp.route',['ngRoute'])
     .config(['$routeProvider',function($routeProvider){
-
-    }]),
+      //配置路由的条件
+      $routeProvider.when('/?', {
+        templateUrl: 'tmps/index-tmp.html'
+      }).when('/nowplaying', {
+        templateUrl: 'tmps/nowplaying-tmp.html',
+        controller:'nowplayingCtrl'
+      }).when('/later', {
+        templateUrl: 'tmps/later-tmp.html',
+        controller:'laterCtrl'
+      }).when('/top250', {
+        templateUrl: 'tmps/top250-tmp.html',
+        controller:'top250Ctrl'
+      })
+    }]);
